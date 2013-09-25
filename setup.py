3ecf114f8e106e704d310b11bafe0498ffbbb193
Dimitri Korsch <korschdima@yahoo.de>
2013-09-25 13:02:47 +0200

    cleaned the repo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pfapi',
-      version='0.0.1',
+      version='0.1.0',
       py_modules=['pfapi'],
       author=['Dimitri Korsch'],
       author_email=['korschdima@gmail.com'],
